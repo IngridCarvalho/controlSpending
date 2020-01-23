30459cd5c0ac2f755257dcf032a352f1b95cc17a
Ingrid Carvalho <ingridcarvalho_1007@hotmail.com>
2020-01-23 10:21:55 -0300

    Made add category and list accounts

--- src/pages/Account/Accounts.js
+++ src/pages/Account/Accounts.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import useGet from '../../utils/useGet';
+
+const url = 'https://control-spending.firebaseio.com/accounts.json';
 
 const Accounts = () => {
+
+    const data = useGet(url);
+   
+    if(data.loading){
+        return <p>Carregando...</p>
+    }
+
     return (
             <table className="table">
                 <thead>
@@ -10,10 +20,18 @@ const Accounts = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Banco</td>
-                        <td>1000,00</td>
-                    </tr>
+                    {
+                        Object
+                            .keys(data.data)
+                            .map(accounts => {
+                                return(
+                                    <tr key={accounts}>
+                                        <td>{data.data[accounts].description}</td>
+                                        <td>{data.data[accounts].balance}</td>
+                                    </tr>
+                                )
+                            })
+                    }
                 </tbody>
             </table>
     )
--- src/pages/Category/AddCategory.js
+++ src/pages/Category/AddCategory.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import usePost from '../../utils/usePost';
+
+const url = 'https://control-spending.firebaseio.com/categories.json';
 
 const AddCategory = () => {
+
+    const [postData, post] = usePost(url); 
+    const [description, setDescription] = useState('');
+
+    const onChangeDescription = evt => {
+        setDescription(evt.target.value);
+    }
+
+    const saveCategory = () => {
+        post({description: description});
+        setDescription('');
+    }
+
     return (
         <React.Fragment>   
             <div className="row">
                 <div className="form-group col-md-5">
                     <label>Informe a descrição da categoria:</label>
-                    <input className="form-control" type="text" />
+                    <input className="form-control" type="text" value={description} onChange={onChangeDescription}/>
                 </div>
                 <div className="col-md-2 mt-3">
-                    <button type="button" className="btn btn-success mt-3">Gravar</button> 
+                    <button type="button" className="btn btn-success mt-3" onClick={saveCategory}>Gravar</button> 
                 </div>
             </div>     
         </React.Fragment>
